ab242ab8ecb08be8b403a04d2cf3ff9d8bcd7ddc
Adrian Lui <luipakhei@gmail.com>
2024-03-24 23:11:16 +0000

    calculator page with table placeholder and stock chart and stock ticker input

---
+++ src/routes/calculator/+page.server.js
@@ -0,0 +1,25 @@
+import { error } from '@sveltejs/kit';
+import { PRIVATE_ALPHA_VANTAGE_API_KEY } from '$env/static/private';
+import { PUBLIC_ALPHA_VANTAGE_URL } from '$env/static/public';
+
+const API_KEY = PRIVATE_ALPHA_VANTAGE_API_KEY
+
+export const load = async ({ url, fetch }) => {
+    const ticker = url.searchParams.get('ticker');
+    try {
+        const res = await fetch(`${PUBLIC_ALPHA_VANTAGE_URL}/query?function=TIME_SERIES_DAILY&symbol=${ticker}&apikey=${API_KEY}`);
+        if (res.ok) {
+            const data = await res.json();
+            return data;
+            // console.log(data);
+            // return {
+            //     props: {
+            //         stockData: data
+            //     }
+            // };
+        }
+    }
+    catch (err) {
+        return error(500, 'Error fetching stock data')
+    }
+};
