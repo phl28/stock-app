29f6dbff9918cfba8606ef2e8d92f5ec4364ab1e
Adrian Lui <luipakhei@gmail.com>
2024-03-24 23:10:01 +0000

    update theme toggle

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -30,17 +30,41 @@
 		</ul>
 	</nav>
 
-	<button on:click={toggle} class="mx-5">
-		<i class="fas fa-moon" id="toggle"></i>
-	</button>
+	<label class="mx-2 flex cursor-pointer gap-2">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="20"
+			height="20"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			><circle cx="12" cy="12" r="5" /><path
+				d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"
+			/></svg
+		>
+		<input type="checkbox" on:click={toggle} class="theme-controller toggle" />
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="20"
+			height="20"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg
+		>
+	</label>
 </header>
 
 <style>
 	header {
 		display: flex;
 		align-items: center;
-		flex-direction: row;
-		justify-content: center;
+		justify-content: space-between;
 	}
 
 	.corner a {
@@ -58,6 +82,7 @@
 	}
 
 	nav {
+		flex-grow: 1;
 		display: flex;
 		justify-content: center;
 		align-items: center;
