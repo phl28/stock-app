97b859d431e412b0927eb0e18d0895f82f5c15d1
Adrian Lui <luipakhei@gmail.com>
2024-04-21 16:28:52 +0100

    remove auth as this app cant be used by anyone else anyways
    this is mainly due to limitation in the API from Futu, IBKR not sure but I reckon something similar

--- src/routes/(authed)/+layout.svelte
+++
@@ -1,5 +0,0 @@
-<slot />
-
-<form method="POST" action="/logout">
-	<button class="btn btn-primary btn-sm">log out</button>
-</form>
--- src/routes/trade-history/+page.svelte
+++ src/routes/trade-history/+page.svelte
@@ -8,3 +8,5 @@
 		content="Allows users to see their past trades and be able to get insights out of it."
 	/>
 </svelte:head>
+
+<h1>Trade History</h1>
