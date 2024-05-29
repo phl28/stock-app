d78552f4432eed68c4fd3ef03b8792d640a9c1de
Adrian Lui <luipakhei@gmail.com>
2024-05-30 00:47:43 +0100

    sign in and signout

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,6 +2,11 @@
 	import { page } from '$app/stores';
 	import logo from '$lib/images/logo-no-background.png';
 	import { theme } from '../../routes/stores.js';
+	import UserButton from 'clerk-sveltekit/client/UserButton.svelte';
+	import SignedIn from 'clerk-sveltekit/client/SignedIn.svelte';
+	import SignedOut from 'clerk-sveltekit/client/SignedOut.svelte';
+	import SignInButton from 'clerk-sveltekit/client/SignInButton.svelte';
+	import { dark } from '@clerk/themes';
 
 	const toggle = () => {
 		theme.toggle();
@@ -18,6 +23,9 @@
 	</div>
 	<nav>
 		<ul>
+			<li aria-current={$page.url.pathname.startsWith('/dashboard') ? 'page' : undefined}>
+				<a href="/dashboard">Dashboard</a>
+			</li>
 			<li aria-current={$page.url.pathname.startsWith('/') ? 'page' : undefined}>
 				<a href="/">Stock calculator</a>
 			</li>
@@ -26,35 +34,49 @@
 			</li>
 		</ul>
 	</nav>
-
-	<label class="mx-2 flex cursor-pointer gap-2">
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="20"
-			height="20"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			stroke-width="2"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			><circle cx="12" cy="12" r="5" /><path
-				d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"
-			/></svg
-		>
-		<input type="checkbox" on:click={toggle} class="theme-controller toggle" />
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="20"
-			height="20"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			stroke-width="2"
-			stroke-linecap="round"
-			stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg
-		>
-	</label>
+	<div class="flex items-center justify-end">
+		<label class="mx-2 flex cursor-pointer gap-2">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="20"
+				height="20"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				><circle cx="12" cy="12" r="5" /><path
+					d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"
+				/></svg
+			>
+			<input type="checkbox" on:click={toggle} class="theme-controller toggle" />
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="20"
+				height="20"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg
+			>
+		</label>
+		<SignedIn>
+			{#if $theme}
+				<UserButton appearance={{ baseTheme: dark }} afterSignOutUrl="/" />
+			{:else}
+				<UserButton afterSignOutUrl="/" />
+			{/if}
+		</SignedIn>
+		<SignedOut>
+			<SignInButton>
+				<button class="btn btn-primary btn-sm">Sign in</button>
+			</SignInButton>
+		</SignedOut>
+	</div>
 </header>
 
 <style>
---
+++ src/routes/sign-in/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import SignIn from 'clerk-sveltekit/client/SignIn.svelte';
+	import { dark, shadesOfPurple, neobrutalism } from '@clerk/themes';
+	import { theme } from '../stores.js';
+</script>
+
+<div class="flex flex-col items-center justify-center">
+	{#if $theme}
+		<SignIn
+			redirectUrl="/trade-history/"
+			appearance={{
+				baseTheme: dark,
+				elements: {
+					footerAction: { display: 'none' }
+				}
+			}}
+		/>
+	{:else}
+		<SignIn redirectUrl="/trade-history/" />
+	{/if}
+</div>
