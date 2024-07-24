e0620f38e3e10ee286105abafb37f5b47167cb44
Adrian Lui <luipakhei@gmail.com>
2024-07-24 21:11:44 +0100

    only logged in user can add trades

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,7 +6,7 @@ import { CLERK_SECRET_KEY } from '$env/static/private'
 export const handle: Handle = sequence(
 	handleClerk(CLERK_SECRET_KEY, {
 		debug: true,
-		protectedPaths: ['/admin'],
+		protectedPaths: ['/trade-history'],
 		signInUrl: '/sign-in',
 	})
 )
