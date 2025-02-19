8e24d10e4357a68af2ded826781be9538cc66e25
Adrian Lui <luipakhei@gmail.com>
2025-02-19 22:07:08 +0000

    more eslint configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,7 @@ module.exports = {
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
+		project: './tsconfig.json',
 		extraFileExtensions: ['.svelte']
 	},
 	env: {
@@ -23,7 +24,11 @@ module.exports = {
 			files: ['*.svelte'],
 			parser: 'svelte-eslint-parser',
 			parserOptions: {
-				parser: '@typescript-eslint/parser'
+				parser: {
+					ts: '@typescript-eslint/parser',
+					js: 'espree',
+					typescript: '@typescript-eslint/parser'
+				}
 			}
 		}
 	]
