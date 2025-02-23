703512dc4bbc7d3be9dcba6c77ac9499a144629b
Adrian Lui <luipakhei@gmail.com>
2025-02-23 20:06:45 +0000

    only authors can edit or see status of their own articles

--- src/routes/articles/[articleId]/+page.server.ts
+++ src/routes/articles/[articleId]/+page.server.ts
@@ -1,13 +1,21 @@
 import { error } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types.js';
+import type { PageServerLoad, RouteParams } from './$types.js';
 
 import { getArticle } from '@/server/db/database';
+import type { AppLocals } from '@/lib/types/utils.js';
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({
+	params,
+	locals
+}: {
+	params: RouteParams;
+	locals: AppLocals;
+}) => {
 	try {
 		const article = await getArticle(Number(params.articleId));
 		return {
-			article: article[0]
+			article: article[0],
+			user: locals.session?.userId
 		};
 	} catch {
 		throw error(404, 'Article not found');
--- tests/e2e/articlesPage.spec.ts
+++ tests/e2e/articlesPage.spec.ts
@@ -2,9 +2,13 @@ import { test, expect } from '@playwright/test';
 
 import { TRADEUP_URL } from './constants';
 
-test.describe('Calculator Page', () => {
+test.describe.configure({ mode: 'serial' });
+
+test.describe('Articles Page', () => {
 	test('Goto Articles Page', async ({ page }) => {
 		await page.goto(`${TRADEUP_URL}/articles/page/1`);
 		await expect(page).toHaveTitle('Trading Articles & Insights');
 	});
+
+	test('Create new article', async ({ page }) => {});
 });
