a8454fd18a58d7f04c9cdb6e134be4fbbab969ec
Adrian Lui <luipakhei@gmail.com>
2025-03-02 22:09:10 +0000

    remove unused file

--- src/lib/components/Grid,.d.ts
+++
@@ -1,4 +0,0 @@
-import type { SvelteComponentTyped } from 'svelte';
-import type { GridProps } from './Grid.svelte';
-
-export default class Grid<TData> extends SvelteComponentTyped<GridProps<TData>> {}
